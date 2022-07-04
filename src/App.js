55ac4c368c691f33b0d0cf690bf2d60c9daa99cb
Trần Văn Hoàng <vanhoang060301@gmail.com>
2022-07-04 14:02:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React nè!
+          Learn React nè!!!
         </a>
       </header>
     </div>
