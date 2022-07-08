e2d9871820f583fa5a0b70157afdf43be3bebd4b
Dansky <lykimphong2001@example.com>
2022-07-08 11:44:56 +0700

    header

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -7,7 +7,6 @@ function Navigation() {
     return(
         <div class="navigation"> 
             <div className="grid">
-<<<<<<< HEAD
                 <Link to="/" style={{textDecoration:"none", color:"#FFFF"}}>
                     <span className="navigation__name">TKB</span>
                 </Link>
@@ -17,19 +16,7 @@ function Navigation() {
                 <Link to="/tuitionFee" style={{textDecoration:"none", color:"#FFFF"}}>
                     <span className="navigation__name">Học phí</span>
                 </Link>
-=======
-                <div className="navCSS">
-                    <Link to="/">
-                        <span className="navigation__name">TKB</span>
-                    </Link>
-                    <Link to="/transcript">
-                        <span className="navigation__name">Bảng điểm</span>
-                    </Link>
-                    <Link to="/tuitionFee">
-                        <span className="navigation__name">Học phí</span>
-                    </Link>
-                </div>
->>>>>>> bb5a371b8bbdb132e8d441acc3d69d6606d86321
+
             </div>   
         </div>
     )
